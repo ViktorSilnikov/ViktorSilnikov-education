8250833b90756c124901c653cccbec1aa19f2c6c
ViktorS <silnikovVA@mail.ru>
2022-06-12 13:31:17 +0300

    вторая HW

---
+++ HomeWork/2/2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int prompt(string message)
+{
+    Console.WriteLine(message);
+    string numStr = Console.ReadLine();
+    int num = int.Parse(numStr);
+    return num;
+}
+int number = prompt("Input a 3 digit number");
+while (number <= 99 || number >= 1000)
+{
+    Console.WriteLine("Input is not a 3 digit number");
+    number = prompt("Input a 3 digit number");
+}
+int mod = number / 100 * 10 + number % 10;
+
+Console.WriteLine($"Without a second digit {number} is {mod}");
---
+++ HomeWork/2/2.3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int power(int foundation, int power)
+{
+    if (power == 0) { return 1; }
+     счетчик int = 1;
+    int ответ = 1;
+    while (счетчик <= мощность)
+    {
+        ответ = ответ * фундамент;
+        счетчик++;
+    }
+    вернуть ответ;
+}
+int abs( номер int)
+{
+    if (number < 0) { number = -число; }
+    возвращаемый номер;
+}
+Console.WriteLine("Введите любое число цифр (желательно более 1000)");
+строка numStr = Console.ReadLine();
+int number = int.Parse(numStr);
+if (number <= 99 && number >= -99) { Console.WriteLine("Недостаточно цифр"); }
+ещё
+{
+    int остаток = abs(число);
+    int цифры = 0;
+    while (остаток != 0)
+    {
+        цифры++;
+        остаток = остаток / 10;
+    }
+    int ThirdDigit = (число / мощность(10, цифры - 3)) % 10;
+    Console.WriteLine($"Третья цифра {number} равна {ThirdDigit}");
+}
