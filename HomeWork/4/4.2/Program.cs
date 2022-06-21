0de7fbdba87bdcb9cc0509e0741364a07436bbe8
ViktorS <silnikovVA@mail.ru>
2022-06-21 15:39:13 +0300

    решена 2я задача 4го семинара

---
+++ HomeWork/4/4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+string istring = Console.ReadLine();
+int inum = int.Parse(istring);
+int lenght = istring.Length;
+int[] arr = new int[lenght];
+int index = 0;
+int sum = 0;
+for (index = 0; index < lenght; index++) ;
+{
+    var tmp = inum % 10;
+    inum = inum / 10;
+    sum = sum + tmp;
+    index = index++;
+}
+Console.Write($"Сумма цифр числа {istring} равна {sum}");
