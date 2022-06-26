efc0e79f5c5e4e1f625ef2641d2a6f022dc54eb4
ViktorS <silnikovVA@mail.ru>
2022-06-26 17:55:13 +0300

    решена 3я задача 5го семинара

---
+++ HomeWork/5/5.3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 3: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] CreateAndFillArr(int large, int min, int max)
+{
+    int[] array = new int[large];
+    for (int ifill = 0; ifill < array.Length; ifill++)
+    {
+        array[ifill] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void printArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int FindDifferenceBetweenMinMax(int[] arr)
+{
+    int min = arr[0];
+    int max = arr[0];
+    int difference = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        else
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    difference = max - min;
+    return difference;
+}
+int[] myArray = CreateAndFillArr(0, 20, 50);
+printArr(myArray);
+var diff = FindDifferenceBetweenMinMax(myArray);
+System.Console.WriteLine($"Difference betwen max and min = , {dif}");
