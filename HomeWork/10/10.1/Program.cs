f8fa454ecd21ff836dfb8efe81b861f58b673bf4
ViktorS <silnikovVA@mail.ru>
2022-07-11 09:32:50 +0300

    решена 2я задача 10го семинара

---
+++ HomeWork/10/10.1/Program.cs
@@ -0,0 +1,36 @@
+﻿// 
+using System.Text;
+void RecursiveNRepeated(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    else
+    {
+        {
+            string str = new StringBuilder($"{N} ".Length * N).Insert(0, $"{N} ", N).ToString();
+            RecursiveNRepeated(N-1);
+            System.Console.Write(str);            
+        }
+    }
+}
+
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int N = Prompt("Введите число");
+    RecursiveNRepeated(N);
+
+}
+
+
+
+Solve();
---
+++ HomeWork/10/10.2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2: Напишите программу вычисления функции 
+//Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+
+int RecursiveAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n+1;
+    }
+    else  
+    {
+        if (n==0)
+        {
+            return RecursiveAkkerman(m - 1, 1);
+        }
+        else
+        {
+            return RecursiveAkkerman(m - 1, RecursiveAkkerman(m, n - 1));
+        }
+    }
+}
+
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int M = Prompt("Введите первый параметр");
+    int N = Prompt("Введите второй параметр");
+    System.Console.WriteLine( RecursiveAkkerman(M, N));
+
+}
+
+
+
+Solve();
