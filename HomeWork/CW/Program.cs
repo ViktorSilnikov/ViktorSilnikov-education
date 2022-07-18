4e189ed16a3d77cfc6e4bca1466a1f664acaf92b
ViktorS <silnikovVA@mail.ru>
2022-07-18 12:18:22 +0300

    написана программа

---
+++ HomeWork/CW/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+string[] array =
+{
+    "hallo",
+    "2",
+    "world",
+   ":-)",
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
