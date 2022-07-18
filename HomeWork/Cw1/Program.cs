a92e693884b5cc514377b72da55a055ccff91bec
ViktorS <silnikovVA@mail.ru>
2022-07-18 15:02:55 +0300

    написана программа

--- HomeWork/Cw1/Program.cs
+++ HomeWork/Cw1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше или равна 3 символа.
+    
+    Console.WriteLine("Введите значение через запятую: ");
+    
+    var input = Console.ReadLine();
+    string[] array = input.Split(",");  
+
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
