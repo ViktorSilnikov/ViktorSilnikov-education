e17c7b6286c2bebb65c0420cffe690a258049ffe
ViktorS <silnikovVA@mail.ru>
2022-07-25 12:18:27 +0300

    добавил подробное описание в ReadMe.md

--- HomeWork/Cw1/Program.cs
+++ HomeWork/Cw1/Program.cs
@@ -1,20 +1,20 @@
 ﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 //длина которых меньше или равна 3 символа.
     
-    Console.WriteLine("Введите значение через запятую: ");
+    Console.WriteLine("Введите значения через запятую: "); //текстовое представление заданного значения
     
-    var input = Console.ReadLine();
-    string[] array = input.Split(",");  
+    var input = Console.ReadLine(); // Объявлена переменная Var
+    string[] array = input.Split(",");  //ввод переменых через запятую
 
 var result = new string[array.Length];
-var realSize = 0;
-foreach (var value in array)
+var realSize = 0; // указан размер
+foreach (var value in array) // перебор массива
 {
-    if (value.Length <= 3)
+    if (value.Length <= 3) //введено при условии задачи 
     {
         result[realSize] = value;
         realSize++;
     }
 }
 
-Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize)); // вывод результата
